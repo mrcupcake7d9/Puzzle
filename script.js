d84e5bfac82857b92d15f516f922d287a99816c4
Mr.Cupcake <mrcupcake7d9@gmail.com>
2025-10-10 22:45:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("button");
+
+button.addEventListener('click', function() {
+    alert("My normal brother, my funking brother he didnt really win the lotery.")
+    setTimeout(() => {
+        alert("Máš u mňa 5€ ti blázon!")
+    }, 2000);
+});
